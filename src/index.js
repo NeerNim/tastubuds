f41b8adb75ec39f9d5ab1de81112d118af5167c5
NeerNim <neernimt@gmail.com>
2019-08-30 20:11:16 +0200

    add h1 content

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-let val = document.getElementById("content");
-console.log(val);
+let contentHolder = document.getElementById("content");
+let h1 = document.createElement("h1");
+contentHolder.appendChild(h1);
+h1.innerText = "Tasty Buds";
